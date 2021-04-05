076197bdbf8612c3301890f0e5ab2949a3eda7f5
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-05 23:27:00 +0900

    add quote local data

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,26 +1,27 @@
 "use strict"
+import {localQuotes} from "./quotes.js"
 
-let apiQuotes = [];
+// let apiQuotes = [];
 
 //Show New Quote
 const newQuote = () => {
     //Pick a random quote from apiQuote array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
     console.log(quote)
 }
 
 //1 get quotes from api
-const getQuotes = async () => {
-    const apiUrl = 'https://type.fit/api/quotes'
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (error) {
-        alert(error.message);
-    }
-}
+// const getQuotes = async () => {
+//     const apiUrl = 'https://type.fit/api/quotes'
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         alert(error.message);
+//     }
+// }
 
 // On Load
 
-getQuotes();
+newQuote();
